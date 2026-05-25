c59bb083501034b5554850f39aa83384ea333109
juantorrezb <ju.torrez@duocuc.cl>
2026-05-25 13:34:13 -0400

    repositoy_service

--- fichaClinica/src/main/java/com/vetnova/fichaClinica/model/FichaClinica.java
+++ fichaClinica/src/main/java/com/vetnova/fichaClinica/model/FichaClinica.java
@@ -1,5 +1,32 @@
 package com.vetnova.fichaClinica.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class FichaClinica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String historial;
     
+    @Column(length = 200, nullable = false)
+    private String diagnostico;
+
+    @Column(length = 200, nullable = false)
+    private String tratamiento;
+
+    @Column(length = 200, nullable = false)
+    private String fecha;
 }
